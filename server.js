4f34063510eaee86deddd3422849eef27b324de1
Allan Leon <aleon.107@outlook.com>
2017-04-14 01:14:42 -0400

    Updated server.js and changed font

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 
-process.env.PWD = process.cwd()
-app.use(express.static(process.env.PWD + '/'));
-
-//app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080, function() {
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
