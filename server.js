675a78f59b2764f3f5f86c787bfd319d8660ee89
Allan Leon <aleon.107@outlook.com>
2017-04-14 01:06:28 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
-app.use("/assets/fonts", express.static(__dirname + '/assets/fonts'));
+process.env.PWD = process.cwd()
+app.use(express.static(process.env.PWD + '/'));
+
+//app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080, function() {
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
